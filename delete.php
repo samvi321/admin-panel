8f0304b3bf932682f5df4c0e5e12e745eb5e398d
samvi321 <68268095+samvi321@users.noreply.github.com>
2020-12-26 11:23:22 +0530

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,11 @@
+<?php
+    session_start();
+    require 'connect.php';
+    if(isset($_POST['deletebtn']))
+    {
+        $id = $_POST['delete_id'];
+        $sql = "DELETE FROM admin WHERE id='$id'";
+        $result = mysqli_query($conn,$sql) or die("query failed.");
+        header("location: ../pages/admin.php");
+    }
+?>
